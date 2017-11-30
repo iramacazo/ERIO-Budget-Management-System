0599d26bff1063d2b960b626432f8a426fcecc0a
iramacazo <ira_macazo@dlsu.edu.ph>
2017-11-30 20:39:11 +0800

    TEST

--- resources/views/addBRF.blade.php
+++ resources/views/addBRF.blade.php
@@ -1,21 +1,31 @@
 <html>
     <head>
         <title> Add BRF </title>
+
+        {{-- JQuery --}}
+        <script src="{{ asset('js\jquery-3.2.1.min.js') }}"></script>
     </head>
     <body>
         <h1> Add Bookstore Requisition Form</h1>
         <p> New Entry </p> <!-- Append INPUT -->
-        <?php
-            $total = 1;
-        ?>
-        <form action="" method="POST">
+        <button id="chook">
+            ADD
+        </button>
+        <form action="" method="POST" id="form">
 
             <!-- APPEND THIS -->
-            <input type="number" name="qty-{{ $total }}" placeholder="Quantity">
-            <input type="description" name="desc-{{ $total }}" placeholder="Description">
+            <input type="number" name="qty[]" placeholder="Quantity">
+            <input type="description" name="desc[]" placeholder="Description">
             <p>Remove</p><br>
 
             <input type="submit" value="submit">
         </form>
     </body>
 </html>
+<script>
+    $(document).ready(function(){
+        $("#chook").click(function () {
+            $("#form").append('<input type="description" name="desc[]" placeholder="Description">');
+        })
+    });
+</script>
