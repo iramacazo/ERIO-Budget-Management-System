791104fa5bad490b1a37d14cf6a1799a8a91b924
iramacazo <ira_macazo@dlsu.edu.ph>
2017-12-06 14:36:38 +0800

    PUSH

--- resources/views/receivePettyCashForm.blade.php
+++ resources/views/receivePettyCashForm.blade.php
@@ -1,16 +1,53 @@
-<html>
-    <head>
-        <title> Receive Petty Cash </title>
-    </head>
-    <body>
-        <h3> Tempo View (Pwede maginig modal)</h3>
-        <h1> Receive Petty Cash </h1>
+@extends('layouts.general_layout')
+
+@section('title', 'Receive Petty Cash')
+
+@section('sidebar')
+    @parent
+    <li><a href="{{ route('accessedAccountsView') }}">Accessed Accounts</a></li>
+    <li><a href="{{ route('brfView') }}">Bookstore Requisition Form</a></li>
+    <li><a href="{{ route('viewMRF') }}"> Material Requisition Form </a></li>
+    <li class="active"><a href="{{ route('pettyCashView') }}">Petty Cash</a></li>
+    <li><a href="{{ route('transacView') }}"> Transactions </a></li>
+@endsection
+
+@section('content')
+    <div class="col s8 offset-s2 white z-depth-2" style="padding: 25px">
+        <h3> Receive Petty Cash </h3>
         <form action="{{ route('receivePettyCash') }}" method="POST">
-            <label>Amount Received: </label>
             {{ csrf_field() }}
-            <input type="number" name="amount_received"><br>
+            <div class="row">
+                <div class="input-field col s6">
+                    <input type="text" class="number" name="amount_received" id="amount">
+                    <label for="amount">Amount</label>
+                </div>
+            </div>
             <input type="hidden" value="{{ $id }}" name="id">
-            <input type="submit" value="Submit">
+            <button type="submit" class="waves-effect waves-light btn green darken-3">Submit</button>
         </form>
-    </body>
-</html>
+    </div>
+@endsection
+
+<script>
+@section('script')
+$(".number").keydown(function (e) {
+    // Allow: backspace, delete, tab, escape, enter and .
+    if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+        // Allow: Ctrl/cmd+A
+        (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+        // Allow: Ctrl/cmd+C
+        (e.keyCode === 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+        // Allow: Ctrl/cmd+X
+        (e.keyCode === 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+        // Allow: home, end, left, right
+        (e.keyCode >= 35 && e.keyCode <= 39)) {
+        // let it happen, don't do anything
+        return;
+    }
+    // Ensure that it is a number and stop the keypress
+    if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+        e.preventDefault();
+    }
+});
+@endsection
+</script>
